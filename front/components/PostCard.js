8aa01eb9061803278b7325954171f2c7d3e4b1b9
minkim94 <alswl8671@gmail.com>
2022-02-20 18:20:22 +0900

    post form

---
+++ front/components/PostCard.js
@@ -0,0 +1,18 @@
+import { Button, Card } from "antd";
+
+const PostCard = () => {
+
+
+
+  return (
+    <div>
+      <Card>
+        <Image />
+        <Content />
+        <Button></Button>
+      </Card>
+    </div>
+  )
+}
+
+export default PostCard;
